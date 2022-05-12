57027d2dbc618a605ff66e214fd95b48c4fe77b0
Yusuf_Abdul <abdul17yusuf@gmail.com>
2022-05-12 02:17:14 +0100

    weather app update(yusufabdul)

--- stem-coders-9/yusuf_abdul/script.js
+++ stem-coders-9/yusuf_abdul/script.js
@@ -9,13 +9,18 @@ var wind = document.querySelector('#wind')
 
 apik = "96a4b810b8d005bf9219b1748108b7b3"
 
+var clear_d = 'https://weather-website-client.tomorrow.io/wp-content/uploads/2021/07/pexels-skitterphoto-3768-1024x575.jpg'
+
+document.body.style.backgroundImage = `url(${clear_d})`;
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundSize = "cover";
 
 function convertion(val){
-    return (val - 273).toFixed(2)
+    return (val - 273).toFixed(0)
 }
 
-    btn.addEventListener('click', function(){
-        fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
+    btn.addEventListener('click',async function(){
+        await fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
         .then(res => res.json())
         .then(data => {
             var nameval = data['name']
@@ -26,13 +31,25 @@ function convertion(val){
             var link = src=`http://openweathermap.org/img/wn/${ convertion(icons)}@2x.png`
             console.log(icons)
             city.innerHTML=`Weather of <span>${nameval}<span>`
-            temp.innerHTML = `Temperature: <span>${ convertion(tempature)} C</span>`
+            temp.innerHTML = `<span>${ convertion(tempature)} ⁰C</span>`
 
-            
+            var tunder_d = ''
+            var tunder_n = ''
+            var clear_d = 'https://weather-website-client.tomorrow.io/wp-content/uploads/2021/07/pexels-skitterphoto-3768-1024x575.jpg'
+            var clear_n = ''
+            var rain_d = ''
+            var rain_n = ''
+            var snow_d = ''
+            var snow_n = ''
+            var mist_d = ''
+            var mist_n = ''
             icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${icons}@2x.png">`
             description.innerHTML = `Sky Conditions: <span>${descrip}<span>`
             wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`
-
+            // document.body.style.backgroundImage = clear_d;
+            document.body.style.backgroundImage = `url(${clear_d})`;
+            document.body.style.backgroundRepeat = "no-repeat";
+            document.body.style.backgroundSize = "cover";
         })
-        .catch(err => alert('You entered Wrong city name'))
+        // .catch(err => alert('You entered Wrong city name'))
     })
